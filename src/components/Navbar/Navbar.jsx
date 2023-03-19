71512615228f05eb83b03f3d8983839cb7d125a2
Janm20 <jan.mackovic@datablast.eu>
2023-03-19 20:57:49 +0100

    something

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between bg-red-300 shadow-md text-black w-full p-4'> 
         <div className='font-lobster text-3xl'>
-            <Link href='/' underline='none' color='inherit'>THE ANNIVERSARY</Link>
+            <Link href='/EmaAnniversary/' underline='none' color='inherit'>THE ANNIVERSARY</Link>
         </div>
         <div className='flex gap-4 font-leaguespartan'>
-            <Link className='hover:text-red-600 hover:underline' href='/photos' underline='none' color="inherit">PHOTOS</Link>
-            <Link className='hover:text-red-600 hover:underline' href='/letter' underline='none' color="inherit">LETTER</Link>
+            <Link className='hover:text-red-600 hover:underline' href='/EmaAnniversary/photos' underline='none' color="inherit">PHOTOS</Link>
+            <Link className='hover:text-red-600 hover:underline' href='/EmaAnniversary/letter' underline='none' color="inherit">LETTER</Link>
         </div>
     </div>
   )
